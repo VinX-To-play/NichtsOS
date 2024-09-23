1ee442c0863a47c62b611451297918fcab9a1c01
VinX-To-play <v@lundborgs.de>
2024-09-23 16:21:15 +0000

    Working home manager

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # home-manager, used for managing user configuration
      home-manager = {
-      #url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
@@ -24,7 +24,6 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-
           # make home-manager as a module of nixos
           # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager {
